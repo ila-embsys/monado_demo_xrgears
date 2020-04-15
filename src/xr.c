3db9f818afb51fd44a5828ebe657adbca75cee98
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-04-15 16:07:51 -0500

    validation fix: Initialize structure

--- src/xr.c
+++ src/xr.c
@@ -256,6 +256,10 @@ _set_up_views(xr_example* self)
 
   self->configuration_views =
     malloc(sizeof(XrViewConfigurationView) * self->view_count);
+  for (uint32_t i = 0; i < self->view_count; ++i) {
+    self->configuration_views[i].type = XR_TYPE_VIEW_CONFIGURATION_VIEW;
+    self->configuration_views[i].next = NULL;
+  }
 
   result = xrEnumerateViewConfigurationViews(
     self->instance, self->system_id, self->view_config_type, self->view_count,
